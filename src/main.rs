f3fda9beaeff7a16996c04dedc3e4fb934cc6d50
Michael Leonard <maybeillrememberit@gmail.com>
2020-12-07 16:57:30 -0600

    Begin adding command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-extern crate serde_json;
+use clap::{Arg, App};
+use serde_json;
 
 pub mod board;
 pub mod piece;
 pub mod serialization;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Chess Engine (Rust)")
+        .version("0.1")
+        .author("Michael Leonard <maybeillrememberit@gmail.com")
+        .about("An experimental chess engine written in Rust")
+        .arg(Arg::with_name("display")
+            .help("Display the given board-file")
+            .takes_value(true))
+        .get_matches();
+
+    let display = matches.value_of("display").unwrap();
+    println!("{}", display);
 }
