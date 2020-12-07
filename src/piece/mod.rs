cfb6ac702a487fb7f0ba93f0e154056d7d4ed512
Michael Leonard <maybeillrememberit@gmail.com>
2020-12-07 00:45:00 -0600

    Start adding Serde implementations
    I have started adding at least the serialization part for some of my
    structs, but I don't really have a good way to test any of this code
    right now other than just testing that the build succeeds.
    
    I am going to take some time out to build out the command line
    interface. It's going to be a necessary addition at some point, but also
    will give me some test flexibility.

--- src/piece/mod.rs
+++ src/piece/mod.rs
@@ -1,5 +1,11 @@
-use super::board::{Coordinate};
+use serde::{Serialize, Deserialize, Serializer};
+use serde_json;
 
+use super::board::{Coordinate};
+use serde::ser::SerializeStruct;
+use crate::board::coordinate::CoordinateAlgebraic;
+
+#[derive(Serialize, Deserialize)]
 pub enum Rank {
     Pawn,
     Bishop,
@@ -10,8 +16,35 @@ pub enum Rank {
 }
 
 pub enum Position {
+    /// Piece was captured by the opponent
     Captured,
+
+    /// Piece is not on the board, but also not captured by the opponent, for example when a pawn
+    /// is promoted
+    OtherwiseOffBoard,
+
+    /// Piece is still on the board, must specify where
     Board(Coordinate),
+}
+
+impl Serialize for Position {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Position", 1)?;
+
+        let serialization = match &self {
+            Position::Captured => { String::from("captured") },
+            Position::OtherwiseOffBoard => { String::from("off") },
+            Position::Board(coord) => {
+                let algebraic = CoordinateAlgebraic::from(*coord);
+                format!("{}{}", algebraic.file(), algebraic.rank())
+            }
+        };
+
+        state.serialize_field("position", &serialization);
+        state.end()
+    }
 }
 
 pub struct Piece {
@@ -22,5 +55,18 @@ pub struct Piece {
 impl Piece {
     pub fn new(rank: Rank, position: Position) -> Piece {
         Piece { rank, position }
+    }
+}
+
+impl Serialize for Piece {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("Piece", 2)?;
+
+        state.serialize_field("rank", &self.rank);
+        state.serialize_field("position", &self.position);
+
+        state.end()
     }
 }
